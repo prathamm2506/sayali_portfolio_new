8e9661a76270d4430b4e137832254c62d3741f68
Pratham Matkar <fs20if005@gmail.com>
2025-09-10 19:56:42 +0530

    project completed on 10-09-2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='py-10 footerline'>
         <hr className='border border-black'/> 
-        <p className="flex flex-row items-center mt-5 gap-2 justify-center text-xl font-bold"><FaCopyright /> 2025 Sayali Nikam. Made with love by<a href="https://www.instagram.com/_design2deploy_/" className='hover:text-gray-800' target='_blank'>Design2Deploy.</a></p>
+        <p className="flex flex-row items-center mt-5 gap-2 justify-center text-xl font-bold">Made with love by<a href="https://www.instagram.com/_design2deploy_/" className='hover:text-gray-800' target='_blank'> Design2Deploy.</a></p>
     </div>
   )
 }
